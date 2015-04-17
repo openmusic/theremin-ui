af0545e9bff3dbed3de2b178543fb3e3d692b65b
sole <sole@mozilla.com>
2015-04-17 15:56:07 +0100

    getting closer

--- demo/main.js
+++ demo/main.js
@@ -1,8 +1,25 @@
 require('./webcomponents-lite.js');
+require('openmusic-oscilloscope').register('openmusic-oscilloscope');
 require('../').register('openmusic-theremin-ui');
 
 var ac = new AudioContext();
-var componentElement = document.createElement('openmusic-theremin-ui');
+var limiter = ac.createDynamicsCompressor();
+// limiter.gain.value = 0.25;
+limiter.connect(ac.destination);
+
+var analyser = ac.createAnalyser();
+var oscilloscope = document.createElement('openmusic-oscilloscope');
+oscilloscope.attachTo(analyser);
+
+analyser.connect(limiter);
+
+var Theremin = require('openmusic-theremin');
+var thereminNode = Theremin(ac);
+thereminNode.connect(analyser);
+thereminNode.start();
+
+var thereminElement = document.createElement('openmusic-theremin-ui');
+thereminElement.attachTo(thereminNode);
 
 document.body.appendChild(componentElement);
-
+document.body.appendChild(oscilloscope);
